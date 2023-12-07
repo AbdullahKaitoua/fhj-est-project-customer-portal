556f42c82e69b514ac7d8aec0cefd60cdd88df89
KaitouAb <kaitouab@esn.at>
2023-12-07 15:27:50 +0100

    upgrade

--- fhj-est-customer-portal/fhj-est-customer-portal/Data/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace fhj_est_customer_portal.Data
-{
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-    {
-    }
-}
---
+++ fhj_est_customer_portal/fhj_est_customer_portal/Services/UserState.cs
@@ -0,0 +1,22 @@
+﻿
+namespace fhj_est_customer_portal.Services
+{
+    public class UserState
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserState(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            FullName = _httpContextAccessor.HttpContext.User.GetFullName();
+        }
+
+        public string FullName { get; private set; }
+
+        public event Action OnChange;
+        public string GetFullName()
+        {
+            return FullName;
+        }
+    }
+}
